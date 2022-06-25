b0204992fecfb55fd91a082619219923745bc1f2
andjipandji99 <andja.anci99@gmail.com>
2022-06-25 14:54:32 +0200

    init

---
+++ src/App.js
@@ -0,0 +1,22 @@
+//import logo from './logo.svg';
+import './App.css';
+
+import {BrowserRouter,Routes,Route,Link} from "react-router-dom";
+
+
+function App() {
+
+
+  return (
+    <BrowserRouter className="App">
+     
+    <Routes>
+    
+    </Routes>
+     
+      
+      </BrowserRouter>
+  );
+}
+
+export default App;
