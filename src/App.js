deecd50a4eae958e44f9666585883b5654e272a7
andjipandji99 <andja.anci99@gmail.com>
2022-06-25 15:01:52 +0200

    ispravka korpi

--- src/App.js
+++ src/App.js
@@ -7,6 +7,39 @@ import Cart from './komponente/Cart';
 import Cenovnik from './komponente/Cenovnik';
 
 function App() {
+  const [cartNum, setcartNum] = useState(0);
+  const [cartProducts, setcartProducts] = useState([]);
+  const [products]=useState([
+    {id:1,
+    title:"Svadbene",
+  description:"Da bi vaše venčanje bilo kao što ste zamislili, posebnu ulogu igra vaša mladenačka torta.",
+amount:0,
+link:"https://ancikolaci.rs/wp-content/uploads/2020/10/Group-25.png.webp"},
+    {id:2,
+      title:"Decije",
+    description:"Obradujte sve koji su na proslavi, a posebno vašeg mališana. Zato smo pripremili raznoliku ponudu torti sa likovima omiljenih crtanih junaka. ",
+  amount:0,
+  link:"https://ancikolaci.rs/wp-content/uploads/2020/10/Group-44.png.webp"},
+    {id:3,
+      title:"Svecane",
+    description:"Domaće torte su izrađene od dnevno svežih jaja i ostalih najkvalitetnijih domaćih i uvoznih materijala i sirovina.Svi filovi su kuvani na pari, i nigde se ne stavljaju sirova jaja, tj. sva su termički obrađena.",
+  amount:0,
+  link:"https://ancikolaci.rs/wp-content/uploads/2021/02/svecane-torte.png.webp"},
+    {id:4,
+      title:"Kolaci",
+    description:"Štanglica u dva sloja, svetlije i tamnije boje, urađen od mlevenog, pečenog lešnika, oraha i keksa. Glaziran crnom čokoladom.",
+  amount:0,
+  link:"https://ancikolaci.rs/wp-content/uploads/2021/02/kolaci.png.webp"},
+  {id:5,
+    title:"Slatki sto",
+  description:"Slatki sto je omiljeni centar okupljanja svake proslave. ",
+amount:0,
+link:"https://ancikolaci.rs/wp-content/uploads/2020/10/Group-47-1.png.webp"
+  }]);
+  function refresCart(){
+  let newProducts=products.filter((prod)=>prod.amount>0);
+  setcartProducts(newProducts);
+  }
 
   function Dodaj(title,id){
     setcartNum(cartNum+1);
@@ -21,6 +54,8 @@ function App() {
 
 
 function Oduzmi(title){
+
+
   if(products.amount>0){
   setcartNum(cartNum-1);
   }else{
